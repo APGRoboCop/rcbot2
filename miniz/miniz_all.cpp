442716a6d51df7694da62ab2d3964480740d0711
R. Hunter <robocop@lycos.co.uk>
2026-04-09 15:26:02 +0100

    Organising VS to analyse SDK2013

--- miniz/miniz_all.cpp
+++ miniz/miniz_all.cpp
@@ -1,3 +1,5 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 /*
  * miniz_all.cpp - Single C++ translation unit wrapper for miniz.
  *
