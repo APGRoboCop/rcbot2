93d7baf52b856443748b35433a985ec9f0303865
R. Hunter <robocop495@gmail.com>
2026-08-06 21:07:18 +0100

    Lowering Linux Opt levels

--- utils/RCBot2_meta/bot_hldm_bot.cpp
+++ utils/RCBot2_meta/bot_hldm_bot.cpp
@@ -427,7 +427,7 @@ bool CHLDMBot :: handleAttack ( CBotWeapon *pWeapon, edict_t *pEnemy )
 			setMoveTo(CBotGlobals::entityOrigin(pEnemy));
 
 		// Ranged firefight: strafe to be a harder target (no-op for melee / cvar off). [APG]RoboCop[CL]
-		doRangedStrafe(pWeapon, pEnemy);
+		//doRangedStrafe(pWeapon, pEnemy);
 
 		if ( pWeapon->getID() == HL2DM_WEAPON_PHYSCANNON && DotProductFromOrigin(m_vAimVector) < rcbot_enemyshoot_gravgun_fov.GetFloat() ) 
 			return true; // keep enemy / don't shoot : until angle between enemy is less than 20 degrees
